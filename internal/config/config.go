1ca7028faa0360f42f4d095e3ae9f5b6be4ab10f
Daniil Shipitsin <danshipitsin@gmaail.com>
2025-01-09 20:47:01 +0200

    1. Commit to create go.mod file

---
+++ internal/config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env-default:"local"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml: "http_server"`
+}
+
+type HTTPServer struct {
+	Adress      string `yaml:"address" env-default:"localhost:8080"`
+	TimeOut     time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"localhost:8080"`
+}
+
+func MustLoad() {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	//check if file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exists: %s", configPath)
+	}
+}
